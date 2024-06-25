41c6f9e088578e1f2e34330830ba89b72c53918b
vlinear0 <vitalliy2005@gmail.com>
2024-06-25 22:07:39 +0300

    initial commit

--- f.hs
+++ f.hs
@@ -1,85 +1,30 @@
-{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
-import System.CPUTime ( getCPUTime )
-
-
-toIntList :: String -> [Int]
-toIntList x = read ("[" ++ x ++ "]")
-
-
-len :: [a] -> Int;
-len [] = 0
-len (_:xs) = succ(len xs);
-
-
-fi :: Eq a => [a] -> a -> Bool;
-fi [] _ = False
-fi (x:xs) el
-        | el == x = True
-        | otherwise = fi xs el;
-
-
-getArr :: Int -> [Int]
-getArr 0 = []
-getArr x = x:getArr (pred x);
-
-
-evenSum :: Integral a => [a] -> a
-evenSum [] = 0
-evenSum (x:xs) = sum (filter even xs)
-
-
-fltr :: (a -> Bool) -> [a] -> [a]
-fltr _ [] = []
-fltr fn (x:xs)
-        | fn x = x:fltr fn xs
-        | otherwise = fltr fn xs;
-
-
-cmpArr :: Integral a => [a] -> [String]
-cmpArr [] = []
-cmpArr (x:xs)
-        | even x = "Even":cmpArr xs
-        | odd x = "Odd":cmpArr xs
-
-
-sFn :: String -> String
-sFn "" = ""
-sFn (x:xs)
-    | x `elem` ['A'..'Z'] = sFn xs
-    | otherwise = x:sFn xs
-
-
-dubArr :: [a] -> [a]
-dubArr [] = []
-dubArr (x:xs) = x:x: dubArr xs;
-
-
-hd :: [a] -> a
-hd [] = error ("Null ptr exception")
-hd (x:xs) = x;
-
-
-fac :: (Integral a, Enum a) => a -> a
-fac 0 = 1
-fac x = x * fac(pred x)
-
-
-main :: IO ()
-main = do
-    start <- getCPUTime
-    let res = evenSum [1..1000000]
-    end <- getCPUTime
-    let diff = fromInteger (end-start)
-    putStrLn $ "Runtime: " ++ show diff;
+import Data.Char (digitToInt)
 
 
 
-{-
+cmpEvenNum :: (Integral a) => a -> a -> a
+cmpEvenNum idx x
+  | even idx = x*2
+  | otherwise = x;
 
-        !Functions that have _type variables_ are called _polymorphic functions_
 
-        ``` hs
-        foo :: a -> a
-        foo x = x;
-        ```
--}
+mult :: (Integral a) => (a -> a -> a) -> [a] -> [a]
+mult fn x = reverse (zipWith fn [1..] (reverse x));
+
+
+readCardNumber :: [Char] -> [Integer]
+readCardNumber "" = []
+readCardNumber (x:xs) = fromIntegral (digitToInt x):readCardNumber xs;
+
+
+divArr :: (Integral a) => [a] -> [a]
+divArr [] = []
+divArr (x:xs)
+  | x > 9 = div x 10 : rem x 10 : divArr xs
+  | otherwise = x:divArr xs
+
+
+validate :: [Char] -> Bool
+validate x
+  | rem (sum (divArr (mult cmpEvenNum (readCardNumber x)))) 10 == 0 = True
+  | otherwise = False
